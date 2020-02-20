73d3de0b98c3ccaaaf53504892602933251fc931
BLTuser <BLTuser@BLT1224>
2020-02-20 13:22:57 +0530

    demo

--- src/test/java/gradledemo/basicdemo.java
+++ src/test/java/gradledemo/basicdemo.java
@@ -1,13 +1,11 @@
 package gradledemo;
-import java.util.concurrent.TimeUnit;
 
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class basicdemo {
 
-	public static void main(String[] args) {
+	
+	
 		@Test
 		 public  void login() {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\BLTuser.BLT1224\\Desktop\\kar\\chromedriver.exe");
